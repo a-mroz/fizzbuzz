1726b0305a172412cd38bd5b09173e7e844588d2
Adam Mróz <sadam36@gmail.com>
2015-10-18 11:15:58 +0200

    Extracting lower and upper bounds to constants; renaming tests

--- src/main/java/com/adam/fizzbuzz/FizzBuzzSolver.java
+++ src/main/java/com/adam/fizzbuzz/FizzBuzzSolver.java
@@ -1,20 +1,27 @@
 package com.adam.fizzbuzz;
 
 public class FizzBuzzSolver {
+
+    public static final int DIVIDER_LOWER_BOUND = 1;
+    public static final int DIVIDER_UPPER_BOUND = 20;
+    public static final int RANGE_LOWER_BOUND = 10;
+    public static final int RANGE_UPPER_BOUND = 100;
+
     public String solve(int divider1, int divider2, int range) {
         validateDivider(divider1);
         validateDivider(divider2);
         validateRange(range);
+
         return "1 2 F 4 B F 7 8 F B";
     }
 
     private void validateRange(int range) {
-        if (range < 10 || range > 100)
+        if (range < RANGE_LOWER_BOUND || range > RANGE_UPPER_BOUND)
             throw new IllegalArgumentException("Range must be within range <%d, %d>");
     }
 
     private void validateDivider(int divider) {
-        if (divider < 1 || divider > 20)
+        if (divider < DIVIDER_LOWER_BOUND || divider > DIVIDER_UPPER_BOUND)
             throw new IllegalArgumentException("Divider must be within range <%d, %d>");
     }
 }
--- src/test/java/com/adam/fizzbuzz/FizzBuzzSolverTest.java
+++ src/test/java/com/adam/fizzbuzz/FizzBuzzSolverTest.java
@@ -3,8 +3,8 @@ package com.adam.fizzbuzz;
 import org.junit.Before;
 import org.junit.Test;
 
+import static com.adam.fizzbuzz.FizzBuzzSolver.*;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 public class FizzBuzzSolverTest {
     private FizzBuzzSolver solver;
@@ -12,43 +12,41 @@ public class FizzBuzzSolverTest {
     @Before
     public void setUp() throws Exception {
         solver = new FizzBuzzSolver();
-
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void firstDividerMustBeGreaterThanLowerBound() throws Exception {
-        solver.solve(-1, 1, 10);
+        solver.solve(DIVIDER_LOWER_BOUND - 1, DIVIDER_LOWER_BOUND, RANGE_LOWER_BOUND);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void firstDividerMustBeLowerThanLowerBound() throws Exception {
-        solver.solve(21, 1, 10);
+    public void firstDividerMustBeLessThanUpperBound() throws Exception {
+        solver.solve(DIVIDER_UPPER_BOUND + 1, DIVIDER_LOWER_BOUND, RANGE_LOWER_BOUND);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void secondDividerMustBeGreaterThanLowerBound() throws Exception {
-        solver.solve(1, -1, 10);
+        solver.solve(DIVIDER_LOWER_BOUND, DIVIDER_LOWER_BOUND - 1, RANGE_LOWER_BOUND);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void secondDividerMustBeLowerThanLowerBound() throws Exception {
-        solver.solve(1, 21, 10);
+    public void secondDividerMustBeLessThanLowerBound() throws Exception {
+        solver.solve(DIVIDER_LOWER_BOUND, DIVIDER_UPPER_BOUND + 1, RANGE_LOWER_BOUND);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void rangeDividerMustBeGreaterThanLowerBound() throws Exception {
-        solver.solve(1, 1, 9);
+        solver.solve(DIVIDER_LOWER_BOUND, DIVIDER_LOWER_BOUND, RANGE_LOWER_BOUND - 1);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void rangeDividerMustBeLowerThanLowerBound() throws Exception {
-        solver.solve(1, 1, 101);
+        solver.solve(DIVIDER_LOWER_BOUND, DIVIDER_LOWER_BOUND, RANGE_UPPER_BOUND + 1);
     }
 
 
     @Test
     public void itWorks() throws Exception {
         assertEquals("1 2 F 4 B F 7 8 F B", solver.solve(3, 5, 10));
-
     }
 }
