c83a8f864243ed5c262e133f23caa9c6df2a2c82
Adam Mróz <sadam36@gmail.com>
2015-10-18 14:33:20 +0200

    Handling exceptional situations in service

--- src/main/java/com/adam/fizzbuzz/service/FizzBuzzServiceImpl.java
+++ src/main/java/com/adam/fizzbuzz/service/FizzBuzzServiceImpl.java
@@ -20,19 +20,37 @@ public class FizzBuzzServiceImpl implements FizzBuzzService {
         Scanner scanner = new Scanner(input);
         StringBuilder sb = new StringBuilder();
 
-        while(scanner.hasNextLine()) {
-
+        while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
             String[] split = line.split("\\s+");
 
-            int divisor1 = Integer.parseInt(split[0]);
-            int divisor2 = Integer.parseInt(split[1]);
-            int range = Integer.parseInt(split[2]);
-
-            sb.append(solver.solve(divisor1, divisor2, range));
+            if (!hasEnoughData(split)) {
+                addErrorMessage(sb);
+            } else {
+                try {
+                    int divisor1 = Integer.parseInt(split[0]);
+                    int divisor2 = Integer.parseInt(split[1]);
+                    int range = Integer.parseInt(split[2]);
+                    sb.append(solver.solve(divisor1, divisor2, range));
+                } catch (NumberFormatException e) {
+                    addErrorMessage(sb);
+                } catch(IllegalArgumentException e) {
+                    sb.append("Error: values out of range. ");
+                    sb.append(e.getMessage());
+                }
+            }
             sb.append("\n");
         }
 
         return sb.toString().trim();
+    }
+
+
+    private boolean hasEnoughData(String[] split) {
+        return split.length == 3;
+    }
+
+    private void addErrorMessage(StringBuilder sb) {
+        sb.append("Error: incorrect data format. Expecting three space separated integers in a line: divider1 divider2 range");
     }
 }
--- src/test/java/com/adam/fizzbuzz/service/FizzBuzzServiceImplTest.java
+++ src/test/java/com/adam/fizzbuzz/service/FizzBuzzServiceImplTest.java
@@ -27,4 +27,31 @@ public class FizzBuzzServiceImplTest {
         assertEquals("1 2 F 4 B F 7 8 F B\n1 F 3 F 5 F B F 9 F 11 F 13 FB 15", service.solveFizzBuzz("3 5 10 \n2 7 15"));
     }
 
+    @Test
+    public void canWorkWithNotOneSpaceSeparatedValues() throws Exception {
+        assertEquals("1 2 F 4 B F 7 8 F B\n1 F 3 F 5 F B F 9 F 11 F 13 FB 15", service.solveFizzBuzz("3   5 \t 10 \n2   7  \t   15"));
+    }
+
+    @Test
+    public void displaysErrorWhenNotEnoughValuesArePassed() throws Exception {
+        assertEquals("1 2 F 4 B F 7 8 F B\nError: incorrect data format. Expecting three space separated integers in a line: divider1 divider2 range", service.solveFizzBuzz("3 5 10\n2 7"));
+    }
+
+    @Test
+    public void displaysErrorWhenTooManyValuesArePassed() throws Exception {
+        assertEquals("Error: incorrect data format. Expecting three space separated integers in a line: divider1 divider2 range", service.solveFizzBuzz("3 5 10 11"));
+    }
+
+    @Test
+    public void displaysErrorWhenNotNumbersArePassed() throws Exception {
+        assertEquals("Error: incorrect data format. Expecting three space separated integers in a line: divider1 divider2 range", service.solveFizzBuzz("a 1 2"));
+        assertEquals("Error: incorrect data format. Expecting three space separated integers in a line: divider1 divider2 range", service.solveFizzBuzz("1 b 2"));
+        assertEquals("Error: incorrect data format. Expecting three space separated integers in a line: divider1 divider2 range", service.solveFizzBuzz("1 2 c"));
+    }
+
+    @Test
+    public void displaysErrorWhenValuesOutOfRangeArePassed() throws Exception {
+        assertEquals("Error: values out of range. Divider must be within range <1, 20>", service.solveFizzBuzz("-1 1 2"));
+        assertEquals("Error: values out of range. Range must be within range <10, 100>", service.solveFizzBuzz("1 1 0"));
+    }
 }
